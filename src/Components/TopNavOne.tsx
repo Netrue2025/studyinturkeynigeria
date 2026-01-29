63f979d4ec3463d56fb39d0fb7cd80cbbf2961d9
juba <juba.jeremiah@gmail.com>
2026-01-29 16:43:53 +0000

    New update

--- src/Components/TopNavOne.tsx
+++ src/Components/TopNavOne.tsx
@@ -1,15 +1,18 @@
 import "../Styles/TopNavOne.css"
+import { BsEnvelope } from "react-icons/bs";
+import { FiPhoneCall } from "react-icons/fi";
+import { CiChat2 } from "react-icons/ci";
 
 export function TopNavOne (){
     return(
         <div className="TopNavOne">
             <figure>
-                <img src="/images/logo.png" alt="logo image" />
+                <img src="/images/logo.jpeg" alt="logo image" />
             </figure>
             <div className="navMenu">
-                <div className="info"><p>info@gmail.com</p></div>
-                <div className="contactUsNow"><p>Contact Us Now</p></div>
-                <div className="question"><p>Questions</p></div>
+                <div className="info"><BsEnvelope className="envelope"/><p>info@gmail.com</p></div>
+                <div className="contactUsNow"><FiPhoneCall className="phone"/><p>Contact Us Now</p></div>
+                <div className="question"><CiChat2 className="chat"/><p>Questions</p></div>
             </div>
         </div>
     )
