4059bbe84a068d371d18195983e610c7186a08b8
juba <juba.jeremiah@gmail.com>
2026-02-06 03:02:59 +0000

    Home page responsiveness completed

--- src/Components/HomeSectionOne.tsx
+++ src/Components/HomeSectionOne.tsx
@@ -38,40 +38,40 @@ export function HomeSectionOne(){
 
 
     return(
-        <div>
-            <section className="HomeSectionOne">
-                <div className="section-one">
-                    <article>
-                        <h2>Study In Turkey.</h2>
-                        <p>There are over 200 universities in Türkiye. The choices are endless. Study in a country with a rich cultural heritage and a 
-                            unique combination of Eastern and Western influences.
-                        </p>
-                    </article>
-                    <article>
-                        <h2>Fully Accredited Universities.</h2>
-                        <p>Come and join more than 320.000 
-                            international students who already study abroad
-                        </p>
-                    </article>
-                </div>
-                <div className="section-two">
-                    <div className="background-container"></div>
-                    <div className="front-image"></div>
-                </div>
-                <div className="section-three">
-                    <article>
-                        <h2>World Class Degree Programmes.</h2>
-                        <p>Study at some of the most prestigious 
-                            universities in the world, and at affordable prices!
-                        </p>
-                    </article>
-                    <article>
-                        <h2>We Offer You Peace of Mind!</h2>
-                        <p>At SPT, our agents will help you find advice on university entry requirements, student visas, tuition fees, scholarships and living costs</p>
-                    </article>
+        
+        <section className="HomeSectionOne">
+            <div className="section-one">
+                <article>
+                    <h2>Study In Turkey.</h2>
+                    <p>There are over 200 universities in Türkiye. The choices are endless. Study in a country with a rich cultural heritage and a 
+                        unique combination of Eastern and Western influences.
+                    </p>
+                </article>
+                <article>
+                    <h2>Fully Accredited Universities.</h2>
+                    <p>Come and join more than 320.000 
+                        international students who already study abroad
+                    </p>
+                </article>
+            </div>
+            <div className="section-two">
+                <div className="background-container"></div>
+                <div className="front-image"></div>
+            </div>
+            <div className="section-three">
+                <article>
+                    <h2>World Class Degree Programmes.</h2>
+                    <p>Study at some of the most prestigious 
+                        universities in the world, and at affordable prices!
+                    </p>
+                </article>
+                <article>
+                    <h2>We Offer You Peace of Mind!</h2>
+                    <p>At SPT, our agents will help you find advice on university entry requirements, student visas, tuition fees, scholarships and living costs</p>
+                </article>
 
-                </div>
-            </section>
-        </div>
+            </div>
+        </section>
+        
     )
 }
