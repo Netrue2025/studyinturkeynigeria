7badb286429072e8e78ba52b2569a30a09d7ac92
juba <juba.jeremiah@gmail.com>
2026-02-07 01:04:54 +0000

    Home page animation and image optimization

--- src/Components/HomeLastSection.tsx
+++ src/Components/HomeLastSection.tsx
@@ -2,6 +2,7 @@ import { CiUser } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import "../Styles/HomeLastSection.css"
 import { FaArrowRightLong } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 export function HomeLastSection(){
     return(
@@ -15,30 +16,37 @@ export function HomeLastSection(){
                     </button>
                 </div>
                 <div className="part-two">
-                    <div className="card-one">
-                        <figure>
-                            <CiUser className="user"/>
-                        </figure>
-                        <h2>Meetings for Students</h2>
-                        <p>
-                            Planning to study in Türkiye? Discuss your options by scheduling a meeting with a representative today.
-                        </p>
-                        <button>
-                            <Link to={"/"} className="link">Setup A Meeting <FaArrowRightLong className="arrow-right"/></Link>
-                        </button>
-                    </div>                    
-                    <div className="card-two">
-                        <figure>
-                            <CiUser className="user"/>
-                        </figure>
-                        <h2>Meetings for Agents</h2>
-                        <p>
-                            Partner with confidence. Schedule a meeting to discuss opportunities, collaboration, and support.
-                        </p>
-                        <button>
-                            <Link to={"/"} className="link">Setup A Meeting <FaArrowRightLong className="arrow-right"/></Link>
-                        </button>
-                    </div>
+                    <motion.div
+                        initial={{ scale: 0.9, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{ duration: 0.5, ease: "easeIn" }}
+                        className="motion"
+                    >
+                        <div className="card-one">
+                            <figure>
+                                <CiUser className="user"/>
+                            </figure>
+                            <h2>Meetings for Students</h2>
+                            <p>
+                                Planning to study in Türkiye? Discuss your options by scheduling a meeting with a representative today.
+                            </p>
+                            <button>
+                                <Link to={"/"} className="link">Setup A Meeting <FaArrowRightLong className="arrow-right"/></Link>
+                            </button>
+                        </div>                    
+                        <div className="card-two">
+                            <figure>
+                                <CiUser className="user"/>
+                            </figure>
+                            <h2>Meetings for Agents</h2>
+                            <p>
+                                Partner with confidence. Schedule a meeting to discuss opportunities, collaboration, and support.
+                            </p>
+                            <button>
+                                <Link to={"/"} className="link">Setup A Meeting <FaArrowRightLong className="arrow-right"/></Link>
+                            </button>
+                        </div>
+                    </motion.div>
                 </div>
             </section>
         </div>
